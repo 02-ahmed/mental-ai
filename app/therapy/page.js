b66f8344991c8689fdd6873fdff9972ddeb81fcd
ahmed <ahmedboabae2@gmail.com>
2024-09-05 17:42:12 +0000

    centre

--- app/therapy/page.js
+++ app/therapy/page.js
@@ -54,7 +54,7 @@ const therapyCenters = [
   },
   {
     name: "Sena Pediatric Therapy",
-    address: "Plot 2103, Adjirigano, First Block Factory",
+    address: "Plot 2103, Adjirigano, First Block Factory, Accra",
     phone: "+233 54 747 6748",
   },
 ];
